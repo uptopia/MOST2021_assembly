44bf81b6597a805a4d035aafb026c47139d8d89a
uptopia <charlotte07221@gmail.com>
2022-05-14 13:56:05 +0800

    dataloaders & modeling

---
+++ dataloaders/__init__.py
@@ -0,0 +1,19 @@
+from dataloaders.datasets import pascal
+from torch.utils.data import DataLoader
+
+def make_data_loader(args, **kwargs):
+
+    if args.dataset == 'pascal':
+        train_set = pascal.VOCSegmentation(args, split='train')
+        val_set = pascal.VOCSegmentation(args, split='val')
+
+        num_class = train_set.NUM_CLASSES
+        train_loader = DataLoader(train_set, batch_size=args.batch_size, shuffle=True, **kwargs)
+        val_loader = DataLoader(val_set, batch_size=args.batch_size, shuffle=False, **kwargs)
+        test_loader = None
+
+        return train_loader, val_loader, test_loader, num_class
+
+    else:
+        raise NotImplementedError
+
