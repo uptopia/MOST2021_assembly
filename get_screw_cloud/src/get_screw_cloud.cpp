ba742a2db054720c4b5f9070710147072d2bc0a3
uptopia <charlotte07221@gmail.com>
2022-05-24 16:09:16 +0800

    generate screw arrow

--- get_screw_cloud/src/get_screw_cloud.cpp
+++ get_screw_cloud/src/get_screw_cloud.cpp
@@ -1,5 +1,6 @@
 #include <ros/ros.h>
 #include <sensor_msgs/PointCloud2.h>
+#include <visualization_msgs/Marker.h>
 #include <pcl_conversions/pcl_conversions.h>
 
 #include <iostream>
@@ -23,7 +24,7 @@ pcl::PointCloud<PointTRGB>::Ptr terminal_pos(new pcl::PointCloud<PointTRGB>);
 pcl::PointCloud<PointTRGB>::Ptr terminal_neg(new pcl::PointCloud<PointTRGB>);
 pcl::PointCloud<PointTRGB>::Ptr organized_cloud_ori(new pcl::PointCloud<PointTRGB>);
 
-ros::Publisher screw_pub;
+ros::Publisher screw_pub, screw_pose_pub;
 sensor_msgs::PointCloud2 screw_msg;
 
 void screw_cb(const part_afford_seg::seg_out::ConstPtr& seg_msg)
@@ -45,6 +46,7 @@ void screw_cb(const part_afford_seg::seg_out::ConstPtr& seg_msg)
         //     << seg_msg->bbox_xmin[i] << " "
         //     << seg_msg->bbox_ymin[i] << " "
         //     << seg_msg->object_name[i] << endl;
+
 
         cv_ptr = cv_bridge::toCvCopy(seg_msg->roi[i], sensor_msgs::image_encodings::TYPE_8UC3);
         cv::Mat screw_color = cv::Mat(cv_ptr->image);
@@ -93,6 +95,43 @@ void screw_cb(const part_afford_seg::seg_out::ConstPtr& seg_msg)
                 }
             }
         }
+
+        PointTRGB arrow_head = organized_cloud_ori->at(x_shift + seg_msg->centers[i].c1_x, y_shift + seg_msg->centers[i].c1_y);
+        PointTRGB arrow_body = organized_cloud_ori->at(x_shift + seg_msg->centers[i].c2_x, y_shift + seg_msg->centers[i].c2_y);
+
+        //=========rviz marker=========
+        Eigen::Vector3d vv1 = Eigen::Vector3d(0.0, 0.0, 0.0);//arrow_head.x, arrow_head.y, arrow_head.z);
+        Eigen::Vector3d vv2 = Eigen::Vector3d(arrow_body.x, arrow_body.y, arrow_body.z);
+        vv2.normalize();
+        Eigen::Quaterniond out = Eigen::Quaterniond::FromTwoVectors(vv1, vv2);
+
+        Eigen::Quaterniond AQ;
+        visualization_msgs::Marker screw_arrow;
+        screw_arrow.header.frame_id = "camera_color_optical_frame";
+        screw_arrow.header.stamp = ros::Time();
+        screw_arrow.ns = "my_namespace";
+        screw_arrow.id = 0;
+        screw_arrow.type = visualization_msgs::Marker::ARROW;
+        screw_arrow.action = visualization_msgs::Marker::ADD;
+        screw_arrow.pose.position.x = arrow_head.x;
+        screw_arrow.pose.position.y = arrow_head.y;
+        screw_arrow.pose.position.z = arrow_head.z;
+        screw_arrow.pose.orientation.x = out.x();
+        screw_arrow.pose.orientation.y = out.y();
+        screw_arrow.pose.orientation.z = out.z();
+        screw_arrow.pose.orientation.w = out.w();
+        screw_arrow.scale.x = 0.030;
+        screw_arrow.scale.y = 0.008;  //width
+        screw_arrow.scale.z = 0.008;  //height
+        screw_arrow.color.a = 1.0;    // Don't forget to set the alpha!
+        screw_arrow.color.r = 0.0;
+        screw_arrow.color.g = 0.0;
+        screw_arrow.color.b = 1.0;
+
+        cout<< "screw_pose rviz marker" <<endl;
+
+        screw_pose_pub.publish(screw_arrow);
+        //=========rviz marker=========
     }
     cout << "screw points " << screw->points.size() <<endl;
     pcl::toROSMsg(*screw, screw_msg);
@@ -134,7 +173,8 @@ int main(int argc, char** argv)
     ros::Subscriber sub_seg = nh.subscribe("/seg_out", 1, screw_cb);
     ros::Subscriber sub_screw_cloud = nh.subscribe("/camera/depth_registered/points", 1, screw_cloud_cb);
     
-    screw_pub = nh.advertise<sensor_msgs::PointCloud2>("screw_topic", 1);
+    screw_pub = nh.advertise<sensor_msgs::PointCloud2>("screw", 1);
+    screw_pose_pub = nh.advertise<visualization_msgs::Marker>("screw_pose", 1);
     ros::spin();
     
     return 0;
