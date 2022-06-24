f58f55d207179f6ebea2c521b000c57b30dcd8db
uptopia <charlotte07221@gmail.com>
2022-06-24 13:59:33 +0800

    unknown modification

--- get_img_info/src/get_img_info.py
+++ get_img_info/src/get_img_info.py
@@ -9,15 +9,22 @@ import message_filters
 from sensor_msgs.msg import Image as msg_Image
 from sensor_msgs.msg import PointCloud2 as msg_Cloud
 from assembly_srv.srv import ImgInfo, ImgInfoResponse
+from assembly_srv.srv import Bboxes, BboxesResponse
+
 
 bridge = CvBridge()
+x_shift = 370
+y_shift = 150
 show_res = True
 n = 1
-curr_img_cropped_msg = []
-curr_cloud_msg = []
 
 class GetImgInfo_Node:
-    def __init__(self): #get_img_info_server
+    def __init__(self, x_shift, y_shift): #get_img_info_server
+        self.x_shift = x_shift
+        self.y_shift = y_shift
+        self.curr_img_cropped_msg = []
+        self.curr_cloud_msg = []
+
         rospy.init_node('get_img_info_server')
         s = rospy.Service('get_img_info', ImgInfo, self.handle_img_info)
         print("get_img_info_server")
@@ -33,12 +40,22 @@ class GetImgInfo_Node:
             sync.registerCallback(self.multiCallback)
             
             if(req.task_type == "motor"):
+                #=======================#
+                # MOTOR grasp pose client 
+                #=======================#
+                motor_response = rospy.wait_for_service('obj_detect', Bboxes)
                 res.grasp_pose = []#motor_grasp_pose
 
             elif(req.task_type == "screw"):
+                #=======================#
+                # SCREW grasp pose client 
+                #=======================#
                 res.grasp_pose = []#screw_grasp_pose
 
             elif(req.task_type == "terminal"):
+                #=======================#
+                # TERMINAL grasp pose client 
+                #=======================#
                 res.grasp_pose = []#terminal_grasp_pose
 
             else:
@@ -49,12 +66,11 @@ class GetImgInfo_Node:
         return res
 
     def multiCallback(self, img_msg, cloud_msg):
-        global curr_img_cropped_msg, curr_cloud_msg, n
 
         #=======================#
         # organized point cloud 
         #=======================#
-        curr_cloud_msg = cloud_msg
+        self.curr_cloud_msg = cloud_msg
 
         #=======================#
         # rgb image -> cropped
@@ -62,22 +78,25 @@ class GetImgInfo_Node:
         bridge = CvBridge()
         cv_image = bridge.imgmsg_to_cv2(img_msg, "bgr8")
 
-        img_cropped = cv_image[150:580,370:1030,:] 
+        img_cropped = cv_image[self.y_shift:580, self.x_shift:1030, :] # y, x, channels
         
         if show_res:
-            # cv2.imshow("ori workspace", cv_image)
             cv2.imshow("select_workspace", img_cropped)
-
             key = cv2.waitKey(1)
+
             if key == ord("s"):
-                file_name = "select_workspace_" + str(n) +".jpg"
-                cv2.imwrite(file_name, img_cropped)
-                print("save " + file_name)
-                n += 1
+                self.save_result(img_cropped)
 
-        curr_img_cropped_msg = bridge.cv2_to_imgmsg(img_cropped, "bgr8")
+        self.curr_img_cropped_msg = bridge.cv2_to_imgmsg(img_cropped, "bgr8")
 
+    def save_result(self, img2save):
+        global n
+
+        file_name = "select_workspace_" + str(n) +".jpg"
+        cv2.imwrite(file_name, img2save)
+        print("save " + file_name)
+        n += 1
 
 if __name__ == "__main__":
-    GetImgInfo_Node()
+    GetImgInfo_Node(x_shift, y_shift)
 
--- obj_detect/src/Det_Node.py
+++ obj_detect/src/Det_Node.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python3
 import cv2
+from matplotlib import testing
 import torch
 import argparse
 import os
-# from matplotlib.pyplot import box
 import rospy
 import sys
 sys.path.insert(1, '/opt/installer/open_cv/cv_bridge/lib/python3/dist-packages/')
@@ -17,7 +17,9 @@ from tool.torch_utils import *
 from tool.darknet2pytorch import Darknet
 
 from obj_detect.msg import bbox, bboxes
+from assembly_srv.srv import Bboxes, BboxesResponse
 
+bridge = CvBridge()
 use_cuda = True
 show_res = True
 n = 1
@@ -34,14 +36,11 @@ def get_args():
     args, unknown = parser.parse_known_args()
     return args
 
-
 class Det_Node:
     def __init__(self, cfg, w, namesfile) -> None:
-        rospy.init_node("Det_Node")
-        self.bridge = CvBridge()
-        rospy.Subscriber("/camera/color/image_raw_workspace", msg_Image, self.imageCallback)
-        self.pub_other_objs = rospy.Publisher('/yolov4_other_objs_bboxes', bboxes, queue_size=10)
-        self.pub_motors = rospy.Publisher('/yolov4_motors_bboxes', bboxes, queue_size=10)
+        rospy.init_node("obj_detect_server")
+        s = rospy.Service('obj_detect', Bboxes, self.handle_obj_detect)
+        print("obj_detect_server")
         self.init_model(cfg, w, namesfile)
         rospy.spin()
 
@@ -52,43 +51,46 @@ class Det_Node:
         self.m = m.cuda()
         self.class_names = load_class_names(namesfile)
 
-    def imageCallback(self, img_msg):
-        cv_image = self.bridge.imgmsg_to_cv2(img_msg, "bgr8")
-        global n 
-        if show_res:
-            res, boxes = self.det(cv_image)
-            boxes = sorted(boxes)
-            cv2.imshow("obj_detect", res)
-            key = cv2.waitKey(1)
-            if key == ord("s"):
-                file_name = "obj_detect_" + str(n)
-                f = open(file_name + ".txt", "w")
-                cv2.imwrite(file_name + ".jpg", cv_image)
-                for bb in boxes[0]:
-                    w = bb[2] - bb[0]
-                    h = bb[3] - bb[1]
-                    x = bb[0] + w/2
-                    y = bb[1] + h/2 
-                    # data = "{} {:.6f} {:.6f} {:.6f} {:.6f}\n".format(bb[6], round(x,6), round(y,6), round(w,6), round(h,6))
-                    data = "{} {:.6f} {:.6f} {:.6f} {:.6f}\n".format(self.class_names[bb[6]], x, y, w, h)
-                    print(data)
-                    f.write(data)
-                n+=1
-                f.close
-        else:
-            self.det(cv_image)
+    def handle_obj_detect(self, req):
+        res = BboxesResponse()
 
-    def det(self, img):
-        sized = cv2.resize(img, (self.m.width, self.m.height))
-        sized = cv2.cvtColor(sized, cv2.COLOR_BGR2RGB)
-        boxes = do_detect(self.m, sized, 0.4, 0.6, use_cuda)
-        self.msg_publish(img, boxes[0])
+        if((req.rgb_img.height > 0) & (req.rgb_img.width > 0)):
 
-        if show_res:
-            result_img = plot_boxes_cv2(img, boxes[0], savename=None, class_names=self.class_names)
-            return result_img, boxes
+            cv_image = self.bridge.imgmsg_to_cv2(req.rgb_img, "bgr8")
 
-    def msg_publish(self, img, boxes):
+            #=======================#
+            # object detection boxes 
+            #=======================#
+            sized = cv2.resize(cv_image, (self.m.width, self.m.height))
+            sized = cv2.cvtColor(sized, cv2.COLOR_BGR2RGB)
+            boxes = do_detect(self.m, sized, 0.4, 0.6, use_cuda)
+            result_img = plot_boxes_cv2(cv_image, boxes[0], savename=None, class_names=self.class_names)
+
+            #=======================#
+            # ros service response 
+            #=======================#
+            bbs_motors, bbs_other_objs = self.srv_response(cv_image, boxes)
+            if(req.task_type == "motor"):
+                res.bboxes = bbs_motors
+            else:
+                res.bboxes = bbs_other_objs
+            res.organized_cloud = req.organized_cloud
+            res.rgb_img = req.rgb_img  
+            res.pixel_shift = req.pixel_shift
+            
+            #=======================#
+            # show, save result 
+            #=======================#
+            if show_res:
+                cv2.imshow("obj_detect", result_img)
+                key = cv2.waitKey(1)
+
+                if key == ord("s"):
+                    boxes = sorted(boxes)
+                    self.save_result(result_img, boxes)
+            
+
+    def srv_response(self, img, boxes):
         width = img.shape[1]
         height = img.shape[0]
         bbs_other_objs = bboxes()
@@ -108,8 +110,25 @@ class Det_Node:
             else:
                 bbs_other_objs.bboxes.append(bb)
 
-        self.pub_motors.publish(bbs_motors)
-        self.pub_other_objs.publish(bbs_other_objs)
+        return bbs_motors, bbs_other_objs
+
+    def save_result(self, img2save, boxes2save):
+        global n
+
+        file_name = "obj_detect_" + str(n)
+        f = open(file_name + ".txt", "w")
+        cv2.imwrite(file_name + ".jpg", img2save)
+        for bb in boxes2save[0]:
+            w = bb[2] - bb[0]
+            h = bb[3] - bb[1]
+            x = bb[0] + w/2
+            y = bb[1] + h/2 
+            # data = "{} {:.6f} {:.6f} {:.6f} {:.6f}\n".format(bb[6], round(x,6), round(y,6), round(w,6), round(h,6))
+            data = "{} {:.6f} {:.6f} {:.6f} {:.6f}\n".format(self.class_names[bb[6]], x, y, w, h)
+            print(data)
+            f.write(data)
+        n+=1
+        f.close
 
 if __name__ == "__main__":
     args = get_args()
