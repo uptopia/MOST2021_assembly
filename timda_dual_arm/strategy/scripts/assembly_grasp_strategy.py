5bf2dedb7f2a13ce39444ca6ee858788afdffc81
uptopia <charlotte07221@gmail.com>
2022-07-04 09:39:13 +0800

    add visiontoArm (result unsure)

--- timda_dual_arm/strategy/scripts/assembly_grasp_strategy.py
+++ timda_dual_arm/strategy/scripts/assembly_grasp_strategy.py
@@ -14,6 +14,10 @@ from arm_control import ArmTask, SuctionTask, Command, Status, RobotiqGripper
 from get_image_info import GetObjInfo
 from math import radians, degrees, sin, cos, pi
 from assembly_srv.srv import GraspPose, GraspPoseRequest
+import sys
+sys.path.insert(1, '/opt/installer/open_cv/cv_bridge/lib/python3/dist-packages/')
+from cv_bridge import CvBridge, CvBridgeError
+import cv2
 
 picture_pose_reached = False
 
@@ -48,9 +52,9 @@ c_pose = {'left' :[[[0.38,  0.2, -0.25],  [0.0, 65, 0.0]],
 class ObjInfo(dict):
     def __init__(self):
         self['id']      = 0
-        self['side_id'] = 'front'         # 'front', 'back', 'side'
+        self['arm_side_id'] = 'front'         # 'front', 'back', 'arm_side'
         self['name']    = 'scratcher' # 'plum_riceball', 'salmon_riceball', 'sandwich', 'burger', 'drink', 'lunch_box'
-        self['state']   = 'new'           # 'new', 'old', 'expired'
+        self['arm_state']   = 'new'           # 'new', 'old', 'expired'
         self['pos']     = None
         self['euler']   = None
         self['sucang']  = 0
@@ -69,7 +73,7 @@ class GraspTask:
 
         self.name = _name
         self.en_sim = en_sim
-        self.state = State.init
+        self.arm_state = State.init
         print("robot init===============================")
         self.dual_arm = DualArmTask(self.name, self.en_sim)
         # self.camara = GetObjInfo()
@@ -91,6 +95,7 @@ class GraspTask:
         self.motor_grasp_pose = Float32MultiArray()
 
         #self.init()
+    
     def visiontoArm(self, rot, tvec, arm_ori):
         dx = -0.04  # unit:meter
         dy = 0.032  # unit:meter
@@ -113,6 +118,7 @@ class GraspTask:
     
         Mat_VecPos_ImgToBase = T0_7 * TransMat_EndToImg * Mat_nVec_Pos
         return Mat_VecPos_ImgToBase
+
     # def motor_grasp_pose(self, grasp_msg):
     #     x = grasp_msg.data[0]
     #     y = grasp_msg.data[1]
@@ -126,64 +132,65 @@ class GraspTask:
 
     #     self.grasp_pose = grasp_msg
 
-    def state_control(self, state, side):
+    def state_control(self, arm_state, arm_side):
         global picture_pose_reached
 
-        print('START', state, side)
-        if state is None:
-            state = State.init
+        print('START', arm_state, arm_side)
+        print('\nCURRENT: arm_state= {}, arm_side = {}'.format(arm_state, arm_side))
+        if arm_state is None:
+            arm_state = State.init
 
-        elif state == State.init:
-            state = State.take_pic
+        elif arm_state == State.init:
+            arm_state = State.take_pic
 
-        elif state == State.take_pic:
-            state = State.approach_motor
+        elif arm_state == State.take_pic:
+            arm_state = State.approach_motor
 
-        elif state == State.approach_motor:
-            state = State.finish_init
+        elif arm_state == State.approach_motor:
+            arm_state = State.finish_init
 
-        elif state == State.finish_init:
-            state = None
-        print('END', state)
-        return state
+        elif arm_state == State.finish_init:
+            arm_state = None
+        print('END', arm_state)
+        return arm_state
      
-    def strategy(self, state, side):
-        print("strategy", state)
+    def strategy(self, arm_state, arm_side):
+        print("strategy", arm_state)
         cmd = Command()
         cmd_queue = queue.Queue()
-        if state == State.init:
+        if arm_state == State.init:
             print("init____________________________")
             cmd['cmd'] = 'jointMove'
             cmd['gripper_cmd'] = 'active'
             cmd['jpos'] = [0, 0, -1.2, 0, 1.87, 0, -0.87, 0] #[0, 0, 0, 0, 0, 0, 0, 0]
-            cmd['state'] = State.init
+            cmd['arm_state'] = State.init
             cmd['speed'] = 20
             cmd_queue.put(copy.deepcopy(cmd))
-            self.dual_arm.send_cmd(side, True, cmd_queue)
+            self.dual_arm.send_cmd(arm_side, True, cmd_queue)
             
-        elif state == State.take_pic:
+        elif arm_state == State.take_pic:
             # cmd['cmd'], cmd['mode'] = 'ikMove', 'line'
-            # # if side == 'left':
-            # #     cmd['pos'], cmd['euler'], cmd['phi'] = c_pose[side][c_pose[side+'_indx']][0], c_pose[side][c_pose[side+'_indx']][1], 0
+            # # if arm_side == 'left':
+            # #     cmd['pos'], cmd['euler'], cmd['phi'] = c_pose[arm_side][c_pose[arm_side+'_indx']][0], c_pose[arm_side][c_pose[arm_side+'_indx']][1], 0
             # # else:
-            # #     cmd['pos'], cmd['euler'], cmd['phi'] = c_pose[side][c_pose[side+'_indx']][0], c_pose[side][c_pose[side+'_indx']][1], 0
-            # cmd['pos'], cmd['euler'], cmd['phi'] = c_pose[side][c_pose[side+'_indx']][0], c_pose[side][c_pose[side+'_indx']][1], 0
+            # #     cmd['pos'], cmd['euler'], cmd['phi'] = c_pose[arm_side][c_pose[arm_side+'_indx']][0], c_pose[arm_side][c_pose[arm_side+'_indx']][1], 0
+            # cmd['pos'], cmd['euler'], cmd['phi'] = c_pose[arm_side][c_pose[arm_side+'_indx']][0], c_pose[arm_side][c_pose[arm_side+'_indx']][1], 0
             # cmd_queue.put(copy.deepcopy(cmd))
             # # cmd['cmd'] = 'occupied'
-            # cmd['state'] = State.apporach_obj
+            # cmd['arm_state'] = State.apporach_obj
             # cmd_queue.put(copy.deepcopy(cmd))
-            # side = self.dual_arm.send_cmd(side, False, cmd_queue)
-            # if side != 'fail':
-            #     c_pose[side+'_indx'] +=1
+            # arm_side = self.dual_arm.send_cmd(arm_side, False, cmd_queue)
+            # if arm_side != 'fail':
+            #     c_pose[arm_side+'_indx'] +=1
             # else:
             #     print('fuckfailfuckfailfuckfail')
 
             print("take_pic____________________________")
             cmd['cmd'], cmd['mode'] = 'ikMove', 'line'
-            cmd['pos'], cmd['euler'], cmd['phi'] = c_pose[side][c_pose[side+'_indx']][0], c_pose[side][c_pose[side+'_indx']][1], 0
+            cmd['pos'], cmd['euler'], cmd['phi'] = c_pose[arm_side][c_pose[arm_side+'_indx']][0], c_pose[arm_side][c_pose[arm_side+'_indx']][1], 0
             cmd_queue.put(copy.deepcopy(cmd))
             # cmd['cmd'] = 'occupied'
-            cmd['state'] = State.take_pic
+            cmd['arm_state'] = State.take_pic
 
             picture_pose_reached = True
             print("hheheheheheheh")
@@ -214,17 +221,43 @@ class GraspTask:
             pitch = self.motor_grasp_pose[4]
             roll = self.motor_grasp_pose[5]
             phi = self.motor_grasp_pose[6]
-            print("motor_grasp_pose Float32MultiArray")
+            print("[cam_coord] motor_grasp_pose Float32MultiArray")
             print(x, y, z, yaw, pitch, roll, phi)
 
+
+            rvecs = np.array([yaw, pitch, roll])
+            tvecs = np.array([x, y, z])
+            rvecs = rvecs.reshape(int(len(rvecs)/3), 3)
+            tvecs = tvecs.reshape(int(len(tvecs)/3), 3)
+            rotations = []
+            rotations = cv2.Rodrigues(rvecs)
+            rotations = rotations.reshape(int(len(rotations)/9), 3, 3)
+            base_H_motor = []
+            fb = self.dual_arm.get_feedback(arm_side)
+            base_H_motor = self.visiontoArm(rotations, tvecs, fb.orientation)
+            print("[robot_coord] motor_grasp_pose Float32MultiArray")
+            print(x, y, z, yaw, pitch, roll, phi)
+
+            self.motor_list = []
+            num = 0
+            self.motor_list[num]['pos'] = base_H_motor[0:3, 3]
+            self.motor_list[num]['vector'] = base_H_motor[0:3, 2]  #aruco_z_axis, rotation (coordinate_axis: aruco z axis)
+            self.motor_list[num]['sucang'], roll = self.dual_arm.suc2vector(base_H_motor[0:3, 2], [0, 1.57, 0])    #TODO #suck ang(0-90), roll (7 axi)
+            self.motor_list[num]['euler'] = [roll, 90, 0] 
+
+            #attach + suc on            
+            cmd['suc_cmd'] = 'On'
+            cmd['cmd'], cmd['mode'] = 'ikMove', 'line' #p2p, line
+            cmd['pos'], cmd['euler'], cmd['phi'] = self.motor_list[0]['pos'], self.motor_list[0]['euler'], 0       
             cmd_queue.put(copy.deepcopy(cmd))
-            side = self.dual_arm.send_cmd(side, False, cmd_queue)
-            if side != 'fail':
-                c_pose[side+'_indx'] +=1
+            
+            arm_side = self.dual_arm.send_cmd(arm_side, False, cmd_queue)
+            if arm_side != 'fail':
+                c_pose[arm_side+'_indx'] +=1
             else:
                 print('fuckfailfuckfailfuckfail')
 
-        elif state == State.approach_motor:
+        elif arm_state == State.approach_motor:
             print("approach_motor____________________________")
             cmd['cmd'], cmd['mode'] = 'ikMove', 'line'
             
@@ -236,24 +269,24 @@ class GraspTask:
             cmd_queue.put(copy.deepcopy(cmd))
             #cmd['suc_cmd'] = 0
             # cmd['cmd'] = 'occupied'
-            cmd['state'] = State.approach_motor
+            cmd['arm_state'] = State.approach_motor
             cmd_queue.put(copy.deepcopy(cmd))
-            side = self.dual_arm.send_cmd(side, False, cmd_queue)
+            arm_side = self.dual_arm.send_cmd(arm_side, False, cmd_queue)
 
             print("movemovemovemovemovemovemovmeovmeovmoemvoemvomevomeovmoemvoemove")
-            if side != 'fail':
-                c_pose[side+'_indx'] +=1
+            if arm_side != 'fail':
+                c_pose[arm_side+'_indx'] +=1
             else:
                 print('fuckfailfuckfailfuckfail')          
 
-        elif state == State.finish_init:
+        elif arm_state == State.finish_init:
             cmd['cmd'] = 'jointMove'
             cmd['gripper_cmd'] = 'reset'
             cmd['jpos'] = [0, 0, 0, 0, 0, 0, 0, 0]
-            cmd['state'] = State.finish_init
+            cmd['arm_state'] = State.finish_init
             cmd_queue.put(copy.deepcopy(cmd))
-            self.dual_arm.send_cmd(side, False, cmd_queue)
-        return side
+            self.dual_arm.send_cmd(arm_side, False, cmd_queue)
+        return arm_side
 
     def process(self):
         rate = rospy.Rate(10)
@@ -261,13 +294,13 @@ class GraspTask:
         while True:
             # l_status = self.dual_arm.left_arm.status
             # if l_status == Status.idle or l_status == Status.occupied:
-            #     l_state = self.state_control(self.dual_arm.left_arm.state, 'left')
+            #     l_state = self.state_control(self.dual_arm.left_arm.arm_state, 'left')
             #     self.strategy(l_state, 'left')
             # rate.sleep()
             #==============================================================================
             r_status = self.dual_arm.right_arm.status
             if r_status == Status.idle or r_status == Status.occupied:
-                r_state = self.state_control(self.dual_arm.right_arm.state, 'right')
+                r_state = self.state_control(self.dual_arm.right_arm.arm_state, 'right')
                 self.strategy(r_state, 'right')
             # rate.sleep()
             # if l_state is None and r_state is None:
