47d626ff88b1f0d96fb74f1d86ee84587083f768
uptopia <charlotte07221@gmail.com>
2022-07-09 09:53:37 +0800

    [CORRECT] screw pose, screw grasp pose checked and is correct!

--- get_screw_terminal/src/get_screw_terminal.cpp
+++ get_screw_terminal/src/get_screw_terminal.cpp
@@ -103,76 +103,80 @@ void screw_cb(const part_afford_seg::seg_out::ConstPtr& seg_msg)
             arrow_body = organized_cloud_ori->at(x_shift + seg_msg->centers[i].c2_x, y_shift + seg_msg->centers[i].c2_y);
             cout<< "arrow_head: " << arrow_head.x << ", " << arrow_head.y << ", " << arrow_head.z << endl;
             cout<< "arrow_body: " << arrow_body.x << ", " << arrow_body.y << ", " << arrow_body.z << endl;
+
+            //=========rviz marker=========
+            Eigen::Vector3d vv1 = Eigen::Vector3d(1.0, 0.0, 0.0);//arrow_head.x, arrow_head.y, arrow_head.z);
+            Eigen::Vector3d vv2 = Eigen::Vector3d(arrow_body.x - arrow_head.x, arrow_body.y - arrow_head.y, arrow_body.z - arrow_head.z);
+            cout << "vv1 = " <<vv1<<endl;
+            cout << "vv2 = " <<vv2<<endl;
+            vv2.normalize();
+            Eigen::Quaterniond out = Eigen::Quaterniond::FromTwoVectors(vv1, vv2);
+            Eigen::Vector3d euler = out.toRotationMatrix().eulerAngles(2, 1, 0);
+            float yaw = euler[0];
+            float pitch = euler[1];
+            float roll = euler[2];
+
+            Eigen::Vector3d vv3 = Eigen::Vector3d(0.0, 0.0, 1.0);
+            Eigen::Quaterniond out1 = Eigen::Quaterniond::FromTwoVectors(vv3, vv2);
+            Eigen::Vector3d euler1 = out1.toRotationMatrix().eulerAngles(2, 1, 0);
+
+            visualization_msgs::Marker screw_arrow;
+            screw_arrow.header.frame_id = "camera_depth_optical_frame";
+            screw_arrow.header.stamp = ros::Time();
+            screw_arrow.ns = "my_namespace";
+            screw_arrow.id = 0;
+            screw_arrow.type = visualization_msgs::Marker::ARROW;
+            screw_arrow.action = visualization_msgs::Marker::ADD;
+            screw_arrow.pose.position.x = arrow_head.x;
+            screw_arrow.pose.position.y = arrow_head.y;
+            screw_arrow.pose.position.z = arrow_head.z;
+            screw_arrow.pose.orientation.x = out.x();
+            screw_arrow.pose.orientation.y = out.y();
+            screw_arrow.pose.orientation.z = out.z();
+            screw_arrow.pose.orientation.w = out.w();
+            screw_arrow.scale.x = 0.050;
+            screw_arrow.scale.y = 0.008;  //width
+            screw_arrow.scale.z = 0.008;  //height
+            screw_arrow.color.a = 1.0;    // Don't forget to set the alpha!
+            screw_arrow.color.r = 1.0;
+            screw_arrow.color.g = 0.0;
+            screw_arrow.color.b = 0.0;
+
+            cout<< "screw_pose rviz marker" <<endl;
+
+            screw_pose_pub.publish(screw_arrow);
+            //=========rviz marker=========
+
+            //=========rviz marker=========
+            visualization_msgs::Marker grasp_arrow;
+            grasp_arrow.header.frame_id = "camera_depth_optical_frame";
+            grasp_arrow.header.stamp = ros::Time();
+            grasp_arrow.ns = "my_namespace";
+            grasp_arrow.id = 0;
+            grasp_arrow.type = visualization_msgs::Marker::ARROW;
+            grasp_arrow.action = visualization_msgs::Marker::ADD;
+            grasp_arrow.pose.position.x = arrow_head.x;
+            grasp_arrow.pose.position.y = arrow_head.y;
+            grasp_arrow.pose.position.z = arrow_head.z;
+            grasp_arrow.pose.orientation.x = out1.x();//pose_msg.orientation.x;
+            grasp_arrow.pose.orientation.y = out1.y();//pose_msg.orientation.y;
+            grasp_arrow.pose.orientation.z = out1.z();//pose_msg.orientation.z;
+            grasp_arrow.pose.orientation.w = out1.w();//pose_msg.orientation.w;
+            grasp_arrow.scale.x = 0.050;//sqrt(pow(coeff_cylinder->values[3],2)+pow(coeff_cylinder->values[4],2)+pow(coeff_cylinder->values[5],2)); //length
+            grasp_arrow.scale.y = 0.008;  //width
+            grasp_arrow.scale.z = 0.008;  //height
+            grasp_arrow.color.a = 1.0;    // Don't forget to set the alpha!
+            grasp_arrow.color.r = 0.0;
+            grasp_arrow.color.g = 0.0;
+            grasp_arrow.color.b = 1.0;
+
+            cout<< "grasp_pose_topic rviz marker" <<endl;
+
+            screw_grasp_pose_pub.publish(grasp_arrow);
+            //=========rviz marker=========
         }
 
-        //=========rviz marker=========
-        Eigen::Vector3d vv1 = Eigen::Vector3d(0.0, 0.0, 0.0);//arrow_head.x, arrow_head.y, arrow_head.z);
-        Eigen::Vector3d vv2 = Eigen::Vector3d(arrow_head.x - arrow_body.x, arrow_head.y - arrow_body.y, arrow_head.z - arrow_body.z);
-        vv2.normalize();
-        Eigen::Quaterniond out = Eigen::Quaterniond::FromTwoVectors(vv1, vv2);
-        Eigen::Vector3d euler = out.toRotationMatrix().eulerAngles(2, 1, 0);
-        float yaw = euler[0];
-        float pitch = euler[1];
-        float roll = euler[2];
-
-        Eigen::Vector3d vv3 = Eigen::Vector3d(0.0, 0.0, 1.0);
-        Eigen::Quaterniond out1 = Eigen::Quaterniond::FromTwoVectors(vv3, vv2);
-        Eigen::Vector3d euler1 = out1.toRotationMatrix().eulerAngles(2, 1, 0);
-
-        visualization_msgs::Marker screw_arrow;
-        screw_arrow.header.frame_id = "camera_depth_optical_frame";
-        screw_arrow.header.stamp = ros::Time();
-        screw_arrow.ns = "my_namespace";
-        screw_arrow.id = 0;
-        screw_arrow.type = visualization_msgs::Marker::ARROW;
-        screw_arrow.action = visualization_msgs::Marker::ADD;
-        screw_arrow.pose.position.x = arrow_body.x;
-        screw_arrow.pose.position.y = arrow_body.y;
-        screw_arrow.pose.position.z = -arrow_body.z;
-        screw_arrow.pose.orientation.x = out.x();
-        screw_arrow.pose.orientation.y = out.y();
-        screw_arrow.pose.orientation.z = out.z();
-        screw_arrow.pose.orientation.w = out.w();
-        screw_arrow.scale.x = 0.050;
-        screw_arrow.scale.y = 0.008;  //width
-        screw_arrow.scale.z = 0.008;  //height
-        screw_arrow.color.a = 1.0;    // Don't forget to set the alpha!
-        screw_arrow.color.r = 1.0;
-        screw_arrow.color.g = 0.0;
-        screw_arrow.color.b = 0.0;
-
-        cout<< "screw_pose rviz marker" <<endl;
-
-        screw_pose_pub.publish(screw_arrow);
-        //=========rviz marker=========
-
-        //=========rviz marker=========
-        visualization_msgs::Marker grasp_arrow;
-        grasp_arrow.header.frame_id = "camera_depth_optical_frame";
-        grasp_arrow.header.stamp = ros::Time();
-        grasp_arrow.ns = "my_namespace";
-        grasp_arrow.id = 0;
-        grasp_arrow.type = visualization_msgs::Marker::ARROW;
-        grasp_arrow.action = visualization_msgs::Marker::ADD;
-        grasp_arrow.pose.position.x = arrow_body.x;
-        grasp_arrow.pose.position.y = arrow_body.y;
-        grasp_arrow.pose.position.z = -arrow_body.z;
-        grasp_arrow.pose.orientation.x = out1.x();//pose_msg.orientation.x;
-        grasp_arrow.pose.orientation.y = out1.y();//pose_msg.orientation.y;
-        grasp_arrow.pose.orientation.z = out1.z();//pose_msg.orientation.z;
-        grasp_arrow.pose.orientation.w = out1.w();//pose_msg.orientation.w;
-        grasp_arrow.scale.x = 0.050;//sqrt(pow(coeff_cylinder->values[3],2)+pow(coeff_cylinder->values[4],2)+pow(coeff_cylinder->values[5],2)); //length
-        grasp_arrow.scale.y = 0.008;  //width
-        grasp_arrow.scale.z = 0.008;  //height
-        grasp_arrow.color.a = 1.0;    // Don't forget to set the alpha!
-        grasp_arrow.color.r = 0.0;
-        grasp_arrow.color.g = 0.0;
-        grasp_arrow.color.b = 1.0;
-
-        cout<< "grasp_pose_topic rviz marker" <<endl;
-
-        screw_grasp_pose_pub.publish(grasp_arrow);
-        //=========rviz marker=========
+        
 
         // //pos, euler, phi
         // //(x, y, z), (), phi
